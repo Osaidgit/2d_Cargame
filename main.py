45ff796148a5c4582008bdd37e4d992bd42a7a59
Osaidgit <161992336+Osaidgit@users.noreply.github.com>
2024-10-26 16:02:03 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,24 @@
 #this is main file
+import pygame as pg
+from world import *
+from node1 import *
+
+
+node =Main()
+run = True
+while run:
+	win.fill(blue)
+	
+	node.event()
+	node.draw()
+	node.update()
+	
+	clock.tick(fps)
+	
+pg.quit()
+quit()
+
+if '__name__' == '__main__':
+	main()
+
+
