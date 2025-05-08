29dd012e0f134446357431c2ddefa88fa7122966
Osaid Ali <161992336+Osaidgit@users.noreply.github.com>
2025-05-08 09:28:07 +0000

    2nd commit

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 import pygame as pg
-win= pg.display.set_mode(0,0),pg.REZIABLE)
-
+win= pg.display.set_mode((0,0),pg.RESIZABLE)
+white = 255,255,255
 while True:
     win.fill(white)
     pg.display.update()
-    
+
